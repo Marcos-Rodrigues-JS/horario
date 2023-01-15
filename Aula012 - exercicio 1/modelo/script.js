1a1d29c5871805051a106e73476faa0f498a8617
Marcos Rodrigues <122711459+Marcos-Rodrigues-JS@users.noreply.github.com>
2023-01-15 02:18:30 -0300

    Update script.js

--- Aula012 - exercicio 1/modelo/script.js
+++ Aula012 - exercicio 1/modelo/script.js
@@ -22,15 +22,15 @@ msg.innerHTML = `Agora são ${hora}:${min}:${sec}`
  if (hora >= 0 && hora < 12){
     //BOM DIA
     document.body.style.background = '#8ad0d8'
-    imagem.src = '/curso em vídeo - Javascript/Aula012 - exercicio 1/imagens/manha.jpg'
+    imagem.src = 'https://marcos-rodrigues-js.github.io/horario/Aula012%20-%20exercicio%201/imagens/manha.jpg'
  } else if (hora >= 12 && hora < 18){
    document.body.style.background = '#6781d5'
     //BOA TARDE
-    img.src = '/curso em vídeo - Javascript/Aula012 - exercicio 1/imagens/tarde.jpg'
+    img.src = 'https://marcos-rodrigues-js.github.io/horario/Aula012%20-%20exercicio%201/imagens/tarde.jpg'
  } else {
    document.body.style.background = '#0a133a'
     //BOA NOITE
-    imagem.src = '/curso em vídeo - Javascript/Aula012 - exercicio 1/imagens/noite.jpg'
+    imagem.src = 'https://marcos-rodrigues-js.github.io/horario/Aula012%20-%20exercicio%201/imagens/noite.jpg'
  }
 }, 1000);
 }
